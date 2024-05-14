5ef0a84977f4f0fb061cbd169414fc62ff014560
Paul Cockrell <paulcockrell@gmail.com>
2024-05-14 15:43:29 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_company
-    puts "hi"
+    puts "hii"
     @current_company ||= current_user.company if user_signed_in?
   end
   helper_method :current_company
